478def9de0faba1da437006e3b56f8f6d841c704
Alpha <85695679+Fahim-XD@users.noreply.github.com>
2022-01-04 18:28:30 +0600

    Create hacking_nasa.py

---
+++ hacking_nasa.py
@@ -0,0 +1,49 @@
+import time
+
+from tqdm import trange
+
+print("HACKING NASA 20%")
+
+time.sleep(2)
+
+print("HACIING NASA 40%")
+
+time.sleep(3)
+
+print("HACKING NASA 60%")
+
+time.sleep(3)
+
+print("HACKING NASA 80%")
+
+time.sleep(3)
+
+print("HACKING NASA 99%")
+
+time.sleep(4)
+
+print("HACKING NASA 100%")
+
+time.sleep(1)
+
+print("SUCCESSFULLY HACKED NASA !")
+
+for i in trange(100):
+
+	time.sleep(0.5)time.sleep(1)
+
+print("Accessing Databases . . . . . . . .")
+
+time.sleep(2)
+
+print("Getting Data. . . . . . . . . .")
+
+time.sleep(1)
+
+print("Copying Data . . . . . . . .")
+
+for i in trange(100):
+
+	time.sleep(0.5)
+
+print("Data Have Been Copied !")
